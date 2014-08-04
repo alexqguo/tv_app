ca5968108e536b9c30cb627eaaf05d7f57b0e89e
Alex Guo <alexqguo@gmail.com>
2014-08-04 14:29:58 -0400

    add shows controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   resources :users, except: :destroy
+  resources :shows, only: :index
   resource :session, only: [:new, :create, :destroy]
 
-  root to: "users#index"
+  root to: "shows#index"
 end
