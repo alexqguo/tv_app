cadf83c524aabb9f4c13c6912c6164c0f5a3f5b9
Alex Guo <alexqguo@gmail.com>
2014-08-04 14:52:52 -0400

    work on search view

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -4,6 +4,8 @@ class ShowsController < ApplicationController
 	end
 
 	def search
+		query = params[:q]
+		@results = query.nil? ? nil : Tmdb::TV.find(query)
 	end
 
 end
--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,2 +1,7 @@
 module ShowsHelper
+
+	def full_image_url(poster_path)
+		poster_path.nil? ? "" : "https://image.tmdb.org/t/p/w185" + poster_path
+	end
+
 end
